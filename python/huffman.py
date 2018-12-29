f83b8568f271a48c7f4817405ae729682a5b9b9e
mike <mike@thesystech.com>
2018-12-29 14:07:04 -0500

    Add static typing

--- python/huffman.py
+++ python/huffman.py
@@ -1,23 +1,22 @@
 from collections import defaultdict, namedtuple
+from typing import Optional, DefaultDict, List, Callable, Dict, Tuple
 import heapq
 
 NULLCHAR = "\x07f"
 
 Node = namedtuple('Node', ['freq', 'c', 'left', 'right'])
 
-
-def leaf(f, c):
+def leaf(f: float, c: str) -> Node:
     return Node(f, c, None, None)
 
-
-def compute_code_tree(s):
-    count = defaultdict(int)
+def compute_code_tree(s: str) -> Optional[Node]:
+    count : DefaultDict[str,int] = defaultdict(int)
     n = 0
     for c in s:
         count[c] += 1
         n += 1
 
-    q = []
+    q : List[Node] = []
     for k, v in count.items():
         heapq.heappush(q, leaf(v / n, k))
 
@@ -27,14 +26,16 @@ def compute_code_tree(s):
     while len(q) > 1:
         r = heapq.heappop(q)
         l = heapq.heappop(q)
-        n = Node(l.freq + r.freq, NULLCHAR, l, r)
-        heapq.heappush(q, n)
+        t = Node(l.freq + r.freq, NULLCHAR, l, r)
+        heapq.heappush(q, t)
 
     return heapq.heappop(q)
 
 
-def dfs(t, f, p):
-    if t == None:
+def dfs(t : Optional[Node],
+        f : Callable[[Optional[Node], List[str]], None],
+        p : List[str]):
+    if t is None:
         return
     else:
         dfs(t.left, f, p + ['0'])
@@ -42,7 +43,7 @@ def dfs(t, f, p):
         dfs(t.right, f, p + ['1'])
 
 
-def compute_code_table(t):
+def compute_code_table(t : Optional[Node]) -> Dict[str,str]:
     r = {}
 
     def visit(n, p):
@@ -53,17 +54,17 @@ def compute_code_table(t):
     return r
 
 
-def gen_huffman(s):
+def gen_huffman(s: str) -> Tuple[Dict[str,str], Optional[Node]]:
     decoder = compute_code_tree(s)
     encoder = compute_code_table(decoder)
     return encoder, decoder
 
 
-def huffman_encode(encoder, s):
+def huffman_encode(encoder: Dict[str,str], s: str) -> str:
     return "".join(encoder[c] for c in s)
 
 
-def decode_char(t, bs):
+def decode_char(t: Node, bs: str) -> Tuple[str, str]:
     if t.c != NULLCHAR:
         return t.c, bs
     else:
@@ -73,7 +74,8 @@ def decode_char(t, bs):
             return decode_char(t.right, bs[1:])
 
 
-def huffman_decode(decoder, bs):
+def huffman_decode(decoder: Optional[Node], bs: str):
+    assert decoder is not None
     r = []
     while bs:
         c, bs = decode_char(decoder, bs)
