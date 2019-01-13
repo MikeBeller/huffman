1117f446a66710a7b5fcc21404d4117b4e8b73c7
mike <mike@thesystech.com>
2019-01-13 15:16:36 -0500

    Fully working version

--- julia/huffman.jl
+++ julia/huffman.jl
@@ -1,4 +1,4 @@
-import DataStructures: BinaryMinHeap, nil, cons
+import DataStructures: BinaryMinHeap, nil, cons, LinkedList, Nil
 
 const NULLCHAR = '\x7f'
 
@@ -9,7 +9,7 @@ mutable struct Node
     right::Union{Node,Nothing}
 end
 
-Base.isless(a::Node, b::Node) = a.freq < b.freq
+Base.isless(a::Node, b::Node) = a.freq < b.freq 
 
 function compute_code_tree(s::String)::Union{Node,Nothing}
     d = Dict{Char,Int}()
@@ -17,15 +17,15 @@ function compute_code_tree(s::String)::Union{Node,Nothing}
         d[c] = get(d, c, 0) + 1
     end
     nodes = Vector{Node}()
-    for (fr,ch) in d
+    for (ch,fr) in d
         push!(nodes, Node(fr, ch, nothing, nothing))
     end
 
     h = BinaryMinHeap(nodes)
     while length(h) > 1
-        n1 = pop!(h)
-        n2 = pop!(h)
-        push!(h, Node(n1.freq + n2.freq, NULLCHAR, n1, n2))
+        r = pop!(h)
+        l = pop!(h)
+        push!(h, Node(l.freq + r.freq, NULLCHAR, l, r))
     end
 
     if length(h) == 0
@@ -35,20 +35,62 @@ function compute_code_tree(s::String)::Union{Node,Nothing}
     end
 end
 
-function compute_code_table(decoder::Union{Node,Nothing})::Dict{Char,String}
+function dfs(t::Union{Node,Nothing}, f::Function, p::LinkedList{Char})
+    if t == nothing
+        return
+    else
+        dfs(t.left, f, cons('0', p))
+        f(t, p)
+        dfs(t.right, f, cons('1', p))
+    end
+end
+
+function compute_code_table(tree::Union{Node,Nothing})::Dict{Char,String}
     r = Dict{Char,String}()
 
-    visit = (n::Node, p::LinkedList{Char}) => 
+    visit = (n::Node, p::LinkedList{Char}) -> 
         if n.c != NULLCHAR
-            r[n.c] = join(p, "")
+            r[n.c] = join(reverse(p), "")
         end
 
-    dfs(t, visit, Nil{Char}())
+    dfs(tree, visit, Nil{Char}())
     r
 end
 
+huffman_encode(encoder, s) = join([encoder[c] for c in s], "")
 
+function huffman_decode_char(tree::Node, bs::String)
+    if tree.c != NULLCHAR
+        tree.c, bs
+    else
+        b,bs = bs[1],bs[2:end]
+        next = if b == '0' tree.left else tree.right end
+        huffman_decode_char(next, bs)
+    end
+end
 
-s = "foo bar baz bee"
-decoder = compute_code_tree(s)
-println(decoder)
+function huffman_decode(decoder::Union{Node,Nothing}, bs::String)
+    if decoder == nothing
+        return ""
+    end
+    r = Vector{Char}()
+    while length(bs) > 0
+        c, bs = huffman_decode_char(decoder, bs)
+        push!(r, c)
+    end
+    join(r, "")
+end
+
+function main()
+    s = "foo bar baz bee"
+    decoder = compute_code_tree(s)
+    encoder = compute_code_table(decoder)
+    println("Encoder:", encoder)
+    ed = huffman_encode(encoder, s)
+    println("Encoded data: ", ed, " Len: ", length(ed))
+    dd = huffman_decode(decoder, ed)
+    println("Decoded data: ", dd, " Len: ", length(dd))
+end
+
+main()
+
